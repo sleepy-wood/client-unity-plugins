9e9f6499d60fb106f0cb84400db99827842a7c6c
PIYoung <24962972+jvvook@users.noreply.github.com>
2022-10-19 14:57:42 +0900

    feat: wip sleep data

--- iOS/UnityPlugin/Sources/UnityPlugin/HealthData.swift
+++ iOS/UnityPlugin/Sources/UnityPlugin/HealthData.swift
@@ -5,6 +5,8 @@ struct HealthData {
     private static let healthStore: HKHealthStore = HKHealthStore()
     private static let typeToRead: HKSampleType = HKSampleType.categoryType(forIdentifier: .sleepAnalysis)!
 
+    private static var sleepSamples: [HKCategorySample] = []
+
     static func isAvailable() -> Bool {
         return HKHealthStore.isHealthDataAvailable()
     }
@@ -31,5 +33,35 @@ struct HealthData {
             // group.leave()
         }
         // group.wait()
+    }
+
+    static func querySleepSamples(startDateInSeconds: Double, endDateInSeconds: Double, maxNumSamples: Int, onSuccess: @escaping SuccessCallback, onError: @escaping ErrorCallback) {
+        let startDate = Date(timeIntervalSince1970: TimeInterval(startDateInSeconds))
+        let endDate = Date(timeIntervalSince1970: TimeInterval(endDateInSeconds))
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)
+        let query = HKSampleQuery(sampleType: typeToRead, predicate: predicate, limit: maxNumSamples, sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+            if let error = error {
+                print("querySleepData error:", error.localizedDescription)
+                onError(error.toInteropError())
+            }
+            if let samples = samples {
+                print("querySleepData samples:", samples)
+                sleepSamples = samples as! [HKCategorySample]
+                onSuccess()
+            }
+        }
+    }
+
+    static func getSleepSamplesCount() -> Int {
+        return sleepSamples.count
+    }
+
+    static func getSleepSamplesIndex(index: Int) -> SleepSample {
+        let sample = sleepSamples[Index(index)]
+        return SleepSample(
+            startDate: sample.startDate.timeIntervalSince1970,
+            endDate: sample.endDate.timeIntervalSince1970,
+            value: sample.value)
     }
 }
--- iOS/UnityPlugin/Sources/UnityPlugin/HealthDataExterns.swift
+++ iOS/UnityPlugin/Sources/UnityPlugin/HealthDataExterns.swift
@@ -9,3 +9,18 @@ func iOS_healthDataIsAvailable() -> Bool {
 func iOS_healthDataRequestAuth(onSuccess: @escaping SuccessBoolCallback, onError: @escaping ErrorCallback) {
     return HealthData.requestAuth(onSuccess: onSuccess, onError: onError)
 }
+
+@_cdecl("iOS_healthDataQuerySleepSamples")
+func iOS_healthDataQuerySleepSamples(startDateInSeconds: Double, endDateInSeconds: Double, maxNumSamples: Int, onSuccess: @escaping SuccessCallback, onError: @escaping ErrorCallback) {
+    return HealthData.querySleepSamples(startDateInSeconds: startDateInSeconds, endDateInSeconds: endDateInSeconds, maxNumSamples: maxNumSamples, onSuccess: onSuccess, onError: onError)
+}
+
+@_cdecl("iOS_healthDataGetSleepSamplesCount")
+func iOS_healthDataGetSleepSamplesCount() -> Int {
+    return HealthData.getSleepSamplesCount()
+}
+
+@_cdecl("iOS_healthDataGetSleepSamplesIndex")
+func iOS_healthDataGetSleepSamplesIndex(index: Int) -> SleepSample {
+    return HealthData.getSleepSamplesIndex(index: index)
+}
