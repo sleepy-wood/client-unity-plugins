d28f13895798c110c766e3549eb7efafdfee7364
PIYoung <24962972+jvvook@users.noreply.github.com>
2022-10-21 15:20:48 +0900

    feat: update

--- Runtime/HealthData.cs
+++ Runtime/HealthData.cs
@@ -162,7 +162,7 @@ namespace NativePlugin.HealthData
         public static DateTime ConvertFromUnixTimestamp(double timestamp)
         {
             DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            return origin.AddSeconds(timestamp);
+            return origin.AddSeconds(timestamp).ToLocalTime();
         }
 
         public static double ConvertToUnixTimestamp(DateTime date)
